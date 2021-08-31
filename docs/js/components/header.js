28bb4ae1c7f423f5dbaefb06c65f28564e0b084c
rgon <rgon@users.noreply.github.com>
2021-08-31 20:02:39 +0000

    deploy: 18dc0a361bf5f7ba7d281d0e0b3b77a2f75fffcd

--- docs/js/components/header.js
+++
@@ -1,8 +0,0 @@
-detectSticky(document.querySelector('header:not(.isfirst)'), (e) => {
-      e.target.classList.remove("expanded")
-})
-
-document.getElementById("expandHeader").addEventListener('click', (event) => {
-    console.log('expand')
-    document.getElementsByTagName("header")[0].classList.toggle("expanded")
-})
