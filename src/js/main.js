0788ac7b3fd310d68ae2aca95adeee63cc56c2e8
Gonzalo Ruiz <rgon@rgon.es>
2021-09-01 14:10:02 +0200

    Remove js logging

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,6 @@ function detectSticky(stickyElem, callBack=undefined) {
         // position: sticky detection: adds .sticking when sticking
         const observer = new IntersectionObserver(
             ([e]) => {
-                console.log(e)
                 // If we don't check here, elements intersecting with the bottom of the screen will also trigger the observer
                 if (e.target.getBoundingClientRect().y <= (parseFloat(window.getComputedStyle(e.target).top) + 1) // Adding threshold to prevent slow scroll bug
                     || e.target.classList.contains('sticking') // Also trigger if it has the sticky class, on Leave
@@ -17,10 +16,7 @@ function detectSticky(stickyElem, callBack=undefined) {
             {threshold: [1]}
         );
         // Check now if the element is sticking (on load): prevents href="#"/coming dack to the tab bug
-        
-        console.log(stickyElem.getBoundingClientRect().y)
-        console.log(parseFloat(window.getComputedStyle(stickyElem).top) + 1)
-        
+                
         if (stickyElem.getBoundingClientRect().y <= (parseFloat(window.getComputedStyle(stickyElem).top) + 1)) {
             stickyElem.classList.add('sticking')
             if (callBack) callBack({target: stickyElem})
