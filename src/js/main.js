fb2c3d4ae481bd5702d02f9d20be376c070b4295
rgon <rgon@outlook.com>
2022-03-12 19:09:33 +0100

    New scroll detection fn

--- src/js/main.js
+++ src/js/main.js
@@ -34,12 +34,15 @@ function detectScroll(elem, callBack=undefined, threshold=1, callbackAtStart=tru
             ([e]) => {
                 console.log(e.target.getBoundingClientRect().y, e.target.innerHTML)
                 // If we don't check here, elements intersecting with the bottom of the screen will also trigger the observer
-                if (e.target.getBoundingClientRect().y <= (window.innerHeight / 4)) {
-                    if (callBack) callBack(e)
+                if (e.target.getBoundingClientRect().y <= -2) {
+                    if (callBack) callBack(Object.assign(e, {'scrolled': true}))
+                } else if (e.target.getBoundingClientRect().y >= 2) {
+                    if (callBack) callBack(Object.assign(e, {'scrolled': false}))
                 }
             },
             {threshold: [threshold]}
-        );
+            );
+        
         // Check now if the element is scrolled by (on load): prevents href="#"/coming dack to the tab bug
         if (elem.getBoundingClientRect().y <= 0 && callbackAtStart) {
             if (callBack) callBack({target: elem})
