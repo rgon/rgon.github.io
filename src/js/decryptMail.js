72da50333ddc710efabc985b89d10d7d86ed8dbc
rgon <rgon@outlook.com>
2022-04-28 03:47:10 +0200

    Changed Vignere encryption modulus, number obfuscation now supported

--- src/js/decryptMail.js
+++ src/js/decryptMail.js
@@ -2,6 +2,10 @@
 Implements simple encryption and decryption of email addresses in order to fool spambots.
 Not intended to be secure, only to keep automated scripts out.
 
+April 2022
+by github.com/rgon
+-----
+
 Maintains the '.tld' and @ symbols so the bots don't think it's encrypted.
 
 If the bots interpret the result of the website, this is of no use, but since that operation
@@ -28,38 +32,53 @@ usage:
 
 // Normally 26 letters in the alphabet, but we can include more with a custom charCodeAt function
 // Uppercase [A-Z] chars have a lower char value, so we could include 32+26 easily without producing non-standard chars
-// TODO
-const vigenereModulus = 26
+// In this case, we're using numbers as well, so 26 + 10 = 36
+const vigenereModulus = 36
+const aOffset = 'a'.charCodeAt()
+const numOffset = '0'.charCodeAt()
 
-function encryptStr (str, pass) { // [a-z]
+function atoi (c) {
+    c = c.charCodeAt()
+    if (c < (26 + aOffset) && c >= aOffset) {
+        return c - aOffset
+    } else {
+        return c - numOffset + 26
+    }
+}
+function itoa (i) {
+    if (i < 26) {
+        return String.fromCharCode(i + aOffset)
+    } else {
+        return String.fromCharCode(i - 26 + numOffset)
+    }
+}
+function encryptStr (str, pass) { // [a-z],[0-9]
     let encString = ''
-    const aOffset = 'a'.charCodeAt()
     str = str.toLowerCase()
     pass = pass.toLowerCase()
 
     // Vigenere cypher implementation
     for (let i=0; i<str.length; i++) {
-        const passCode = pass[i % pass.length].charCodeAt() - aOffset
+        const passCode = atoi(pass[i % pass.length])
 
         // Caesar cypher sequence
-        encString += String.fromCharCode((str[i].charCodeAt() - aOffset + passCode) % vigenereModulus + aOffset)
+        encString += itoa((atoi(str[i]) + passCode) % vigenereModulus)
     }
 
     return encString
 }
 function decryptStr (str, pass) {
     let decString = ''
-    const aOffset = 'a'.charCodeAt()
     str = str.toLowerCase()
     pass = pass.toLowerCase()
 
     // Vigenere cypher implementation
     for (let i=0; i<str.length; i++) {
-        const passCode = pass[i % pass.length].charCodeAt() - aOffset
-        const decypheredChar = (str[i].charCodeAt() - aOffset - passCode)
+        const passCode = atoi(pass[i % pass.length])
+        const decypheredChar = (atoi(str[i]) - passCode)
         
         // Now that we subtract the passCode, we may get negative numbers. Roll back to the last char in that case
-        decString += String.fromCharCode((decypheredChar > 0 ? decypheredChar : decypheredChar + vigenereModulus) % vigenereModulus + aOffset)
+        decString += itoa((decypheredChar > 0 ? decypheredChar : decypheredChar + vigenereModulus) % vigenereModulus)
     }
 
     return decString
