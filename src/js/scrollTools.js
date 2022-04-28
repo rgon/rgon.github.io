13dff3a960984139a9d39fdea6da2ab5266de4bb
rgon <rgon@outlook.com>
2022-04-28 02:01:28 +0200

    Blogpost revamp: remove phone jump and fix readingIndex

--- src/js/scrollTools.js
+++ src/js/scrollTools.js
@@ -32,16 +32,15 @@ function detectSticky(stickyElem, callBack=undefined) {
 function detectScroll(elem, callBack=undefined, threshold=1, callbackAtStart=true) { // threshold: percentage of the element that has to be scrolled past
     if (elem) {
         const observer = new IntersectionObserver(
-            ([e]) => {
-                console.log(e.target.getBoundingClientRect().y, e.target.innerHTML)
-                // If we don't check here, elements intersecting with the bottom of the screen will also trigger the observer
-                if (e.target.getBoundingClientRect().y <= -2) {
-                    if (callBack) callBack(Object.assign(e, {'scrolled': true}))
-                } else if (e.target.getBoundingClientRect().y >= 2) {
-                    if (callBack) callBack(Object.assign(e, {'scrolled': false}))
-                }
-            },
-            {threshold: [threshold]}
+                ([e]) => {
+                    // If we don't check here, elements intersecting with the bottom of the screen will also trigger the observer
+                    if (e.target.getBoundingClientRect().y <= -2) {
+                        if (callBack) callBack(Object.assign(e, {'scrolled': true}))
+                    } else if (e.target.getBoundingClientRect().y >= 2) {
+                        if (callBack) callBack(Object.assign(e, {'scrolled': false}))
+                    }
+                },
+                {threshold: [threshold]}
             );
         
         // Check now if the element is scrolled by (on load): prevents href="#"/coming dack to the tab bug
