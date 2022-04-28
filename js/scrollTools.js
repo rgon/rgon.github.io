2106f3c799e9082a513a0493b1899c53ef5404e8
rgon <rgon@users.noreply.github.com>
2022-04-28 13:19:24 +0000

    deploy: beaf19d66752f223fd712914132c1ed29938c99f

--- js/scrollTools.js
+++ js/scrollTools.js
@@ -5,58 +5,68 @@ For page sections and sticky headers.
 April 2022
 by github.com/rgon
 -----
+
+detectSticky options:
+    {
+        threshold -> array of [% of item height in [0, 1]]: 1=full item, 0.5=half height
+            A threshold of zero triggers the intersection when the first pixel of the target element intersects the root element. 
+            A threshold of one triggers when the entire target element is inside the root element.
+        rootMargin -> t r b l: '0px 0px -50% 0px' = triggers when passing half the screen
+            does not work if the root element is not an actual element on the page, such as the viewport.
 */
 
 // @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3-or-Later
 
 // Because the handleIntersection callback is run on the main thread, be careful to only perform logic that is absolutely necessary and keep this logic lean.
-function detectSticky(stickyElem, callBack=undefined) {
+function detectSticky(stickyElem, callBack=undefined, options=undefined) {
     if (stickyElem) {
         // position: sticky detection: adds .sticking when sticking
         const observer = new IntersectionObserver(
-            ([e]) => {
-                // If we don't check here, elements intersecting with the bottom of the screen will also trigger the observer
-                if (e.target.getBoundingClientRect().y <= (parseFloat(window.getComputedStyle(e.target).top) + 1) // Adding threshold to prevent slow scroll bug
-                    || e.target.classList.contains('sticking') // Also trigger if it has the sticky class, on Leave
-                ) {
-                    e.target.classList.toggle('sticking', e.intersectionRatio < 1)
+            (entries, observer) => {
+                entries.forEach(e => {
+                    e.target.classList.toggle('sticking', e.isIntersecting)
                     if (callBack) callBack(e)
-                }
+                })
             },
-            {threshold: [1]}
+            options ? options : {
+                rootMargin: '0px 0px -100% 0px', // Boundary to a 0px line in the top. Thus, always intersects, no top: -1px hack needed.
+                threshold: 0
+            }
         );
-        // Check now if the element is sticking (on load): prevents href="#"/coming dack to the tab bug
-                
-        if (stickyElem.getBoundingClientRect().y <= (parseFloat(window.getComputedStyle(stickyElem).top) + 1)) {
-            stickyElem.classList.add('sticking')
-            if (callBack) callBack({target: stickyElem})
-        } else {
-            stickyElem.classList.remove('sticking') // Also trigger if it has the sticky class, on Leave
-        }
     
-        observer.observe(stickyElem)
+        if (!!stickyElem.forEach) {
+            stickyElem.forEach(element => {
+                observer.observe(element)
+            });
+        } else {
+            observer.observe(stickyElem)
+        }
     }
 }
 
-function detectScroll(elem, callBack=undefined, threshold=1, callbackAtStart=true) { // threshold: percentage of the element that has to be scrolled past
+function detectScroll(elem, callBack=undefined, options=undefined, callbackAtStart=true) { // threshold: percentage of the element that has to be scrolled past
     if (elem) {
         const observer = new IntersectionObserver(
-                ([e]) => {
-                    // If we don't check here, elements intersecting with the bottom of the screen will also trigger the observer
-                    if (e.target.getBoundingClientRect().y <= -2) {
-                        if (callBack) callBack(Object.assign(e, {'scrolled': true}))
-                    } else if (e.target.getBoundingClientRect().y >= 2) {
-                        if (callBack) callBack(Object.assign(e, {'scrolled': false}))
-                    }
+                (entries, observer) => {
+                    entries.forEach(e => {
+                        if (callBack) callBack(Object.assign(e, {'scrolled': e.boundingClientRect.top < e.intersectionRect.top}))
+                    })
                 },
-                {threshold: [threshold]}
+                options ? options : {threshold: [0, 0.5]} // by default, if scrolled past half
             );
         
+        /*
         // Check now if the element is scrolled by (on load): prevents href="#"/coming dack to the tab bug
         if (elem.getBoundingClientRect().y <= 0 && callbackAtStart) {
             if (callBack) callBack({target: elem})
-        }
+        }*/
     
-        observer.observe(elem)
+        if (!!elem.forEach) {
+            elem.forEach(e => {
+                observer.observe(e)
+            });
+        } else {
+            observer.observe(elem)
+        }
     }
 }
